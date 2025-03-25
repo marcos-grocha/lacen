b3ac6eacfb15883b802e9cbf257a0617a47e65c5
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-03-25 15:05:22 -0300

    Adiciona conteúdo do componente à view perguntas frequentes

--- src/views/FAQView.vue
+++ src/views/FAQView.vue
@@ -1,6 +1,10 @@
 <template>
-  <h3 style="text-align: center;">FAQ</h3>
+  <div class="homepage">
+    <h1 style="text-align: center;">FAQ</h1>
+    <FAQLacen />
+  </div>
 </template>
 
 <script setup>
+  import FAQLacen from '@/components/FAQLacen.vue';
 </script>
