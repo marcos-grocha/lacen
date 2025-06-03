8756d1ec581b182b49c7d9b5daf72aa258eebd3f
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-06-03 06:01:12 -0300

    refactor: path to fetch profile pictures

--- src/views/ProfessionalView.vue
+++ src/views/ProfessionalView.vue
@@ -138,7 +138,7 @@
       id: 1, 
       name: 'Fulano Ciclano 1', 
       role: 'Biomédico',
-      photo: new URL('@/assets/homem-default.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/homem-default.png', import.meta.url).href,
       bio: 'Profissional especializado em análises clínicas, com mais de 10 anos de experiência na área de diagnóstico laboratorial e controle de qualidade.',
       website: 'https://exemplo.com',
       email: 'fulano1@fsph.se.gov.br'
@@ -147,7 +147,7 @@
       id: 2, 
       name: 'Fulano Ciclano 2', 
       role: 'Biomédica',
-      photo: new URL('@/assets/mulher-default.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/mulher-default.png', import.meta.url).href,
       bio: 'Especialista em biologia molecular e microbiologia, com foco em pesquisas inovadoras e desenvolvimento de novos métodos diagnósticos.',
       email: 'fulano2@fsph.se.gov.br'
     },
@@ -155,7 +155,7 @@
       id: 3, 
       name: 'Marcos Guimarães', 
       role: 'Desenvolvedor de Software',
-      photo: new URL('@/assets/gmarcos-dev.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/gmarcos-dev.png', import.meta.url).href,
       bio: 'Desenvolvedor especialista em Ruby on Rails, construindo soluções web através de Desenvolvimento Orientado a Testes (TDD) e práticas ágeis. Apaixonado pelo processo de aprendizagem em novas tecnologias e motivado a buscar excelência em suas soluções.',
       website: 'https://gmarcos-dev.surge.sh/',
       email: 'gmarcos.dev@gmail.com'
@@ -172,7 +172,7 @@
       id: 5, 
       name: 'Fulano Ciclano 5', 
       role: 'Desenvolvedora Front-End',
-      photo: new URL('@/assets/mulher-default.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/mulher-default.png', import.meta.url).href,
       bio: 'Desenvolvedora full-stack com experiência em Vue.js, Node.js e bancos de dados SQL/NoSQL, focada em criar interfaces modernas e intuitivas.',
       website: 'https://portfolio-exemplo.com',
       email: 'fulano5@fsph.se.gov.br'
@@ -181,7 +181,7 @@
       id: 6, 
       name: 'Fulano Ciclano 6', 
       role: 'Biomédico',
-      photo: new URL('@/assets/homem-default.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/homem-default.png', import.meta.url).href,
       bio: '',
       email: 'fulano6@fsph.se.gov.br'
     },
@@ -197,7 +197,7 @@
       id: 8, 
       name: 'Fulano Ciclano 8', 
       role: 'Biomédica',
-      photo: new URL('@/assets/mulher-default.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/mulher-default.png', import.meta.url).href,
       bio: '',
       email: 'fulano8@fsph.se.gov.br'
     },
@@ -205,7 +205,7 @@
       id: 9, 
       name: 'Fulano Ciclano 9', 
       role: 'Biomédica',
-      photo: new URL('@/assets/mulher-default.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/mulher-default.png', import.meta.url).href,
       bio: '',
       email: 'fulano9@fsph.se.gov.br'
     },
@@ -229,7 +229,7 @@
       id: 12, 
       name: 'Fulano Ciclano 12', 
       role: 'Biomédico',
-      photo: new URL('@/assets/homem-default.png', import.meta.url).href,
+      photo: new URL('@/assets/img/perfil-user/homem-default.png', import.meta.url).href,
       bio: '',
       email: 'fulano12@fsph.se.gov.br'
     }
